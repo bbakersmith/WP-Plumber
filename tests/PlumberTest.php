64ba31f7933875f9aec6adbcec61cf040d1a2a75
Ben Baker-Smith <bbakersmith@gmail.com>
2013-11-16 12:41:43 -0600

    plumber tests created but not written

---
+++ tests/PlumberTest.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once('../lib/wp-plumber/Plumber.php');
+
+
+class PlumberTest extends PHPUnit_Framework_TestCase {
+
+  public function testRouteArgFormation() {
+
+  }
+
+  public function testRouteTemplateCascade() {
+
+  }
+
+  public function testPodArgumentFormation() {
+
+  }
+
+  public function testArgArrayFormation() {
+
+  }
+
+  public function testPreprocessorCalling() {
+
+  }
+
+}
+
+
+?>
